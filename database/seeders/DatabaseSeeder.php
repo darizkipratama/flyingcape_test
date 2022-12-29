b0f4e123bcbb7f5870f54a833c0fe7258c663341
darizki-pratama <dpratama@gmail.com>
2022-12-29 19:47:09 +0700

    update readme.md

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,10 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory(5)->create();
         \App\Models\Product::factory(20)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'SuperBuyer',
+            'email' => 'superbuyer@example.com',
+            'phone_number' => '0818555555'
+        ]);
     }
 }
